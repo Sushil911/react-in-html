a292dba3a902859571c6bcb3f6a6cf2cb5a90e4c
Sushil911 <Sushilbasyal30@gmail.com>
2024-12-07 16:09:00 +0545

    Todo-List

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,37 +1,54 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from "react";
 
-const App = () => {
-  const [todo, settodo] = useState([])
-  return (
+function App() {
+
+    console.log("localstorage", localStorage.getItem("todo"));
+    const [todo, setTodo] = useState(
+        localStorage.getItem("todo") ? JSON.parse(localStorage.getItem("todo")) : []
+    );
+
+    useEffect(() => {
+        
+        if (todo) {
+            localStorage.setItem("todo", JSON.stringify(todo));
+        }
+    },[todo])
+
+    return (
     <div>
-    <div className='m-8'>
-    <h1>Todo App</h1>
-    <input type="text" className='bg-purple-400' onKeyDown={(event)=> {
-      if (event.code==="Enter") {
-        console.log("Enter pressed");
-        settodo([...todo,event.target.value])
-        event.target.value=""
-      }
-  }} />
-  <br />
-  {
-    todo.map((value,index)=>{
-      return <div key={index} className='mt-2 flex justify-between'>
-        <div>
-        {index+1}. {value}
-        </div>
-        <div className='text-red-500 cursor-pointer' onClick={()=> {
-          const newTodoList=todo.filter((value,i)=>{
-            return (i!==index) ? true : false;
-          })
-          settodo(newTodoList)
-        }}>Delete</div>
-      </div>
-    })
-  }
-    </div>
-    </div>
-  )
+        <h1 className="underline text-3xl">Todo App</h1>
+            <input type="text" className="bg-purple-300" onKeyDown={
+                function (event) {
+                    console.log(event)
+                    if (event.code === "Enter") {
+                        // logic..
+                        // add todo/
+
+                        console.log("enter pressed")
+                        setTodo([...todo, event.target.value]);
+                        event.target.value=""
+                    }
+                    
+                }
+            } />
+
+            {todo.map( (value, index)=> {
+                return <div key={index} className="shadow p-5 mt-5 flex justify-between">
+                    <div>
+                        {value}
+                    </div>
+                    
+                    <div className="text-red-500 cursor-pointer" onClick={ ()=> 
+                        setTodo([...todo.filter( (value, i)=>  (i !== index))])
+                     }>
+                        Delete
+                    </div>
+                   
+                </div>
+            })}
+
+
+    </div>);
 }
 
-export default App
+export default App;
